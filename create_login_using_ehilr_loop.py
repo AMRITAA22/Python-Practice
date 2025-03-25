3cead1b501575af3302961f25d2831e431a59b13
AMRITAA22 <amritaa2202@gmail.com>
2025-03-25 19:36:49 +0530

    Delete

--- create_login_using_ehilr_loop.py
+++
@@ -1,9 +0,0 @@
-username='amrita'
-password='a@20005'
-user=input('enter username: ')
-passw=input("enter password: ")
-while username!=user or password!=passw:
-    user=input('enter username: ')
-    passw=input("enter password: ")
-else:
-    print("it match")
