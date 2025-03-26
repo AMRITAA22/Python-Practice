c988596e3afa154b668681f0d61c260bd0ef63a0
AMRITAA22 <amritaa2202@gmail.com>
2025-03-26 13:25:06 +0530

    new commit

---
+++ diff_use_of_format_fun.py
@@ -0,0 +1,8 @@
+name='amrita'
+sal=50000
+age=18
+print("{} sal is {} age is {}".format(name, age, sal))
+print("{} sal is {} age is {}".format(age,sal,name))
+print("{} sal is {} age is {}".format(sal,name,age))
+A=1
+print(f"{A} is a good boy")
