ad7f15a2fdc8bada995b2872befc32f1d3dd72e1
AMRITAA22 <amritaa2202@gmail.com>
2025-03-26 13:23:11 +0530

    new commit

---
+++ count_no_of_vowels_consonant_from_input_string.py
@@ -0,0 +1,10 @@
+name=input("enter a string")
+vowel=0
+consonant=0
+for i in name:
+    if i=='a' or i=='e' or i=='i' or i=='o' or i=='o' or i=='u':
+        vowel+=1
+    else:
+        consonant+=1
+print("vowel: ",vowel)
+print("consonant",consonant)
