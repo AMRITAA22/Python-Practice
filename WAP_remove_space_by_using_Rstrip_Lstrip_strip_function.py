d0f4b28a1b2e9be88502f3f7f2ab5e7de8278c05
AMRITAA22 <amritaa2202@gmail.com>
2025-03-30 13:03:52 +0530

    COMMIT

---
+++ WAP_remove_space_by_using_Rstrip_Lstrip_strip_function.py
@@ -0,0 +1,10 @@
+p=input("enter your programming name: ")
+p_name=p.strip()
+if p_name=='python':
+    print(p_name)
+elif p_name=='java':
+    print(p_name)
+elif p_name=='cpp':
+    print(p_name)
+else:
+    print("wrong programming name")
